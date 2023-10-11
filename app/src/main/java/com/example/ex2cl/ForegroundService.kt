4e7ad0315b2bc6ca85848612f017c59718a8e9a6
mmg1111-spec <idry123412@gmail.com>
2023-10-11 10:15:20 +0900

    upload

--- app/src/main/java/com/example/ex2cl/ForegroundService.kt
+++ app/src/main/java/com/example/ex2cl/ForegroundService.kt
@@ -3,36 +3,27 @@ package com.example.ex2cl
 import android.app.Notification
 import android.app.Service
 import android.content.Intent
-import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
 import android.os.IBinder
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
-import androidx.core.app.ActivityCompat.startActivityForResult
+import androidx.core.app.NotificationCompat
+
 
 class ForegroundService : Service() {
-    private lateinit var mediaProjection: MediaProjection
 
     override fun onCreate() {
         super.onCreate()
-        // ForegroundService 클래스의 onCreate 메서드 내에서
+
         val mediaProjectionManager =
             getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-        val permissionIntent = mediaProjectionManager.createScreenCaptureIntent()
-        val REQUEST_MEDIA_PROJECTION = null
-        REQUEST_MEDIA_PROJECTION?.let { startActivityForResult(permissionIntent, it) }
-
-        // MediaProjection을 초기화하고 시작하는 로직 추가
-    }
-
-    private fun startActivityForResult(permissionIntent: Intent?, requestMediaProjection: Intent) {
+        mediaProjectionManager.createScreenCaptureIntent()
 
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        // 포그라운드 서비스로 설정
+
         startForeground(1, createNotification())
 
         return START_STICKY
@@ -42,32 +33,20 @@ class ForegroundService : Service() {
         return null
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        // MediaProjection 및 미디어 레코딩 리소스 해제
-    }
-
     private fun createNotification(): Notification {
-        // 포그라운드 서비스를 나타내는 알림 생성
         val channelId = "ForegroundServiceChannel"
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                "Foreground Service Channel",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
-
-        val notification = Notification.Builder(this, channelId)
-            .setContentTitle("Foreground Service")
-            .setContentText("Running")
-            .setSmallIcon(R.mipmap.ic_launcher)
+        val channel = NotificationChannel(
+            channelId,
+            "Foreground Service Channel",
+            NotificationManager.IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
+        return NotificationCompat.Builder(this, channelId)
+            .setContentTitle("")
+            .setContentText("")
+            .setPriority(NotificationCompat.PRIORITY_LOW)
             .build()
-
-        return notification
     }
 }
--- app/src/main/java/com/example/ex2cl/MainActivity.kt
+++ app/src/main/java/com/example/ex2cl/MainActivity.kt
@@ -1,25 +1,23 @@
 package com.example.ex2cl
+
 import android.app.Activity
-import android.app.Presentation
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.media.MediaRecorder
 import android.media.MediaRouter
 import android.media.projection.MediaProjection
 import android.media.projection.MediaProjectionManager
-import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.view.Display
 import android.view.Surface
-import android.view.SurfaceView
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
-import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.ServerSocket
@@ -27,25 +25,22 @@ import java.net.Socket
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var startProjectionButton: Button
     private lateinit var stopProjectionButton: Button
-    private lateinit var foregroundButton: Button
-    private lateinit var startServerButton : Button
-    private lateinit var startClientButton : Button
-    private lateinit var stopServerButton : Button
+    private lateinit var startServerButton: Button
+    private lateinit var startClientButton: Button
+    private lateinit var stopServerButton: Button
     private var mediaProjection: MediaProjection? = null
     private var mediaRecorder: MediaRecorder? = null
     private var mediaRouter: MediaRouter? = null
-    private var presentation: Presentation? = null
-    private var isForegroundMode = false
+    private var presentation: CustomPresentation? = null
     private lateinit var requestMediaProjectionLauncher: ActivityResultLauncher<Intent>
     private var serverSocket: ServerSocket? = null
     private var clientSocket: Socket? = null
     private var serverRunning = false
     private var clientRunning = false
-    private lateinit var presentationView: SurfaceView
     private lateinit var presentationSurface: Surface
+    private var isPermissionRequested = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,51 +48,64 @@ class MainActivity : AppCompatActivity() {
 
         startProjectionButton = findViewById(R.id.startProjectionButton)
         stopProjectionButton = findViewById(R.id.stopProjectionButton)
-        foregroundButton = findViewById(R.id.foregroundButton)
         startServerButton = findViewById(R.id.startServerButton)
         startClientButton = findViewById(R.id.startClientButton)
         stopServerButton = findViewById(R.id.stopServerButton)
-       // presentationView = findViewById(R.id.presentationView)
-      //  presentationSurface = presentationView.holder.surface
-       // presentation = display?.let { CustomPresentation(this, it, presentationSurface) }
 
         val serviceIntent = Intent(this, ForegroundService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(serviceIntent)
-        } else {
-            startService(serviceIntent)
-        }
+        startForegroundService(serviceIntent)
+
         requestMediaProjectionLauncher =
             registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
                 if (result.resultCode == Activity.RESULT_OK) {
                     val data: Intent? = result.data
                     if (data != null) {
-                        // Permission granted, handle media projection
+
                         handleMediaProjectionResult(data)
                     }
                 } else {
-                    // Permission denied, handle accordingly
-                    Toast.makeText(this, "Media projection permission denied", Toast.LENGTH_SHORT)
-                        .show()
+
+                    Toast.makeText(this, "미디어 프로젝션 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
                 }
             }
+
+
         startProjectionButton.setOnClickListener {
-            startMediaProjection()
+            if (!isPermissionRequested) {
+                requestMediaProjection()
+            }
         }
 
         stopProjectionButton.setOnClickListener {
             stopMediaProjection()
         }
 
-        foregroundButton.setOnClickListener {
-            toggleForegroundMode()
-        }
         startServerButton.setOnClickListener {
             startServer()
         }
 
         startClientButton.setOnClickListener {
-            startClient()
+
+            val alertDialogBuilder = AlertDialog.Builder(this)
+            alertDialogBuilder.setTitle("Enter Server IP Address")
+            val ipAddressEditText = EditText(this)
+            ipAddressEditText.hint = "Server IP Address"
+            alertDialogBuilder.setView(ipAddressEditText)
+            alertDialogBuilder.setPositiveButton("확인") { _, _ ->
+                val serverIpAddress = ipAddressEditText.text.toString()
+                if (serverIpAddress.isNotEmpty()) {
+
+                    startClient(serverIpAddress)
+                } else {
+                    Toast.makeText(this, "서버 IP 주소를 입력하세요.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            alertDialogBuilder.setNegativeButton("취소") { dialog, _ ->
+                dialog.dismiss()
+            }
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+
         }
 
         stopServerButton.setOnClickListener {
@@ -107,35 +115,14 @@ class MainActivity : AppCompatActivity() {
         mediaRouter = getSystemService(Context.MEDIA_ROUTER_SERVICE) as MediaRouter
     }
 
-    private fun stopServer() {
-        serverRunning = false
-        serverSocket?.close()
-        clientSocket?.close()
-    }
-
-    private fun startClient() {
-        if (!clientRunning) {
-            thread {
-                try {
-                    val serverAddress = "192.168.0.6" // 서버의 IP 주소를 지정
-                    val serverPort = 8080 // 서버 포트를 지정
-
-                    clientSocket = Socket(serverAddress, serverPort)
-
-                    val input = BufferedReader(InputStreamReader(clientSocket?.getInputStream()))
-                    while (clientRunning) {
-                        val receivedData = input.readLine() // 서버로부터 데이터 수신
-
-                        // receivedData를 Presentation에 표시하는 로직 추가
-                        // Presentation에 미디어 데이터를 표시하는 방법은 CustomPresentation 클래스에 따라 달라질 수 있음
-                    }
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
+    private fun requestMediaProjection() {
+        if (mediaProjection == null) {
+            val mediaProjectionManager =
+                getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
+            val permissionIntent = mediaProjectionManager.createScreenCaptureIntent()
+            requestMediaProjectionLauncher.launch(permissionIntent)
         }
     }
-
     private fun startServer() {
         if (!serverRunning) {
             thread {
@@ -145,70 +132,81 @@ class MainActivity : AppCompatActivity() {
                     while (serverRunning) {
                         val client = serverSocket?.accept()
                         clientSocket = client
-                        if (client != null) {
-                            // 클라이언트와 통신하는 코드 작성
-                            // 예: 클라이언트로부터 미디어 데이터를 전송받고 Presentation에 표시
-                        }
                     }
                 } catch (e: IOException) {
                     e.printStackTrace()
+                } finally {
+                    try {
+                        serverSocket?.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
                 }
             }
         }
+    }
+    private fun startClient(serverIpAddress: String) {
+        if (!clientRunning) {
+            thread {
+                var clientSocket: Socket? = null
+                try {
+                    val serverPort = 8080 // 서버 포트를 지정
+
+                    clientSocket = Socket(serverIpAddress, serverPort)
+
+                    val input = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
+                    while (clientRunning) {
+                        input.readLine()
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } finally {
+                    try {
+                        clientSocket?.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun stopServer() {
+        serverRunning = false
+        serverSocket?.close()
+        clientSocket?.close()
     }
 
     private fun handleMediaProjectionResult(data: Intent) {
         val mediaProjectionManager =
             getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
         mediaProjection = mediaProjectionManager.getMediaProjection(Activity.RESULT_OK, data)
-
-        // Now that you have the media projection, you can start using it for screen capturing or other media tasks
         startMediaRecording()
+        
     }
 
     private fun startMediaRecording() {
         if (mediaProjection != null) {
             mediaRecorder = MediaRecorder()
-
-            // Configure MediaRecorder settings
-            mediaRecorder?.setVideoSource(MediaRecorder.VideoSource.SURFACE)
-            mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-
-            // Set the output file path
-            //   val outputFilePath = "/path/to/your/output/file.mp4"
-            val outputFileName = "sample_video.mp4"
-            val externalDir = Environment.getExternalStorageDirectory() // 외부 저장소의 최상위 디렉토리를 가져옵니다.
-            val outputFilePath = File(externalDir, outputFileName).absolutePath
-            mediaRecorder?.setOutputFile(outputFilePath)
-
-            // Set other necessary configurations (e.g., video size, bit rate, etc.)
-
             try {
-
-                mediaRecorder?.prepare()
-                mediaRecorder?.start()
-            } catch (e: IOException) {
+                mediaRecorder?.setVideoSource(MediaRecorder.VideoSource.SURFACE)
+            } catch (e: IllegalStateException) {
                 e.printStackTrace()
+                Toast.makeText(this, "비디오 소스 설정 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                return
             }
-        }
-    }
-
-    private fun startMediaProjection() {
-        if (mediaProjection == null) {
-            // 미디어 프로젝션을 시작하기 전에 미디어 프로젝션 권한을 얻는 Intent를 생성합니다.
-            val mediaProjectionManager =
-                getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-            val permissionIntent = mediaProjectionManager.createScreenCaptureIntent()
-
-            // 권한 요청을 실행하고 결과를 처리합니다.
-            requestMediaProjectionLauncher.launch(permissionIntent)
-            val REQUEST_MEDIA_PROJECTION = 1 // 이전에는 null로 초기화되었으므로 변경합니다.
-            startActivityForResult(permissionIntent, REQUEST_MEDIA_PROJECTION)
-        } else {
-            // 이미 미디어 프로젝션을 실행 중인 경우 처리할 내용 추가
+            try {
+                mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                mediaRecorder?.setVideoEncoder(MediaRecorder.VideoEncoder.H264)
+            } catch (e: IllegalStateException) {
+                e.printStackTrace()
+                Toast.makeText(this, "비디오 인코더 설정 중 오류가 발생했습니다.", Toast.LENGTH_SHORT).show()
+                return
+            }
 
         }
     }
+
 
     private fun stopMediaProjection() {
         if (mediaProjection != null) {
@@ -223,21 +221,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun toggleForegroundMode() {
-        if (!isForegroundMode) {
-            // 포그라운드 모드 활성화
-            presentation?.show()
-            isForegroundMode = true
-        } else {
-            // 포그라운드 모드 비활성화
-            presentation?.dismiss()
-            isForegroundMode = false
-        }
-    }
-
-    // Presentation을 사용하여 미디어를 프로젝션하는 방법에 대한 예제 코드
     private fun showMediaOnPresentation(display: Display) {
-        presentation = CustomPresentation(this, display, presentationSurface)
+        if (presentation == null) {
+            presentation = CustomPresentation(this, display, presentationSurface)
+        }
         presentation?.show()
     }
 
@@ -245,23 +232,5 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         stopMediaProjection()
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        val REQUEST_MEDIA_PROJECTION = null
-        if (requestCode == REQUEST_MEDIA_PROJECTION) {
-            if (resultCode == Activity.RESULT_OK && data != null) {
-                // 미디어 프로젝션 권한을 얻었으므로 ForegroundService에 전달
-                val serviceIntent = Intent(this, ForegroundService::class.java)
-                serviceIntent.putExtra("mediaProjectionData", data)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    startForegroundService(serviceIntent)
-                } else {
-                    startService(serviceIntent)
-                }
-            } else {
-                // 권한을 얻지 못한 경우 처리
-            }
-        }
-    }
 }
+
